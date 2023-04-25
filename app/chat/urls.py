d9febf31c783a14ca006eb1ecd81d6886c75d380
Dobatoo <t_kimura_pvt_001214@yahoo.co.jp>
2023-04-25 18:06:25 +0900

    added chat templates basis

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'chat' #for namespacing of urls
 urlpatterns = [
     path('', views.chatpage, name='chatpage'),
+
+
+    #for development below, remove before launch
+    path('mock/', views.mock, name='mock'),
 ]
--- app/chat/views.py
+++ app/chat/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def chatpage(request):
     return HttpResponse("chat world")
+
+#for development below, remove before launch
+def mock(request):
+    return render(request, 'chat/index.html')
