cdac980813e40a83d72490c3aff26f45dc687ba9
NR-Kyoto <nagaya.rei.62r@st.kyoto-u.ac.jp>
2023-04-29 02:01:20 +0900

    [update]よさげな情報のみを返すようにした

--- app/evaluation/views.py
+++ app/evaluation/views.py
@@ -1,42 +1,95 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import language_tool_python
 
-check_categories = ['CONFUSED_WORDS', 'GRAMMAR', 'REPETITIONS']
+from dataclasses import dataclass # 構造体
+from deep_translator import GoogleTranslator # 翻訳
+import language_tool_python # 文法チェック
+
+check_categories = ['CONFUSED_WORDS', 'GRAMMAR', 'REPETITIONS', 'TYPOS']
+
+@dataclass(init=False)
+class error:
+
+    # error_type: int
+    index: int # 対象文の番号
+    sentence: str
+    start: int # 文内での開始位置
+    end: int # 文内での終了位置
+    message: str # エラーメッセージ
+    suggestion: str # 正しい文
+
+    def __init__(self, index, match, text):
+        # error_type = 
+        self.index = index
+        self.sentence = match.sentence
+        self.start = match.offset - text.index(match.sentence)
+        self.end = self.start + match.errorLength
+        self.message = GoogleTranslator(source='auto',target='ja').translate(match.message)
+        self.suggestion = match.sentence[:self.start] + match.replacements[0] + match.sentence[self.end:]
+
+    def to_html(self):
+        html = '<div>'
+        html += '<p>' + self.sentence[:self.start]
+        html += '<strong>' + self.sentence[self.start:self.end] + '</strong>'
+        html += self.sentence[self.end:] + '</p>'
+        html += '<p>' + self.message + '</p>'
+        html += '<p>fixed : ' + self.suggestion + '</p>'
+        html += '</div>'
+        return html
 
 def evaluationpage(request):
 
-    # sentance = 'Last weak, I goed to the librery to borrow some bookes. I was very hungary so I eated some pizza on the way. When I arrived at the librery, I realized that I left my library card at home. So, I borowed a book without checking it out. The librery staff was very mad at me and asked me to leave immediately. I said I was sorry, but they didn\'t accepted my apology. After that, I goed home and sleeped for a few hours.'
-    sentance = 'I goed to the market yesterdy to buy some apple. But they were very expensive, so I couldn\'t bought them. After that, I went to a cafe to eat a sandwich and drinked some coffee. While I was eating, I saw an old friend of mine. I goed to talk to him and we had a nice conversation. He told me that he had buyed a new car and he was very happy with it. After the conversation, I goed home and watched a movie. The movie was very boring, so I sleeped halfway through.'
+    sentances = [
+        "Hey, did you was at the party last night?",
+        "No, I didn't went. I was study for my exam.",
+        "Oh, that's right. I forgot about your exam. Did you did well?",
+        "Yes, I think I did good. I answered all of the questions correctly.",
+        "That's great! So, did you went home right after the exam?",
+        "No, I went to the mall to buy some clothes. I falled in love with a jacket that I saw there.",
+        "Ah, I see. Did you bought the jacket?",
+        "Yes, I did. It was expensive, but it was worth it.",
+        "Sounds like you had a busy day yesterday. I was stayed at home and watched TV all day.",
+        "That sounds relaxing. What did you watched?",
+        "I watched a movie called 'Jurassic Park'. It was very exciting.",
+        "Oh, I have saw that movie before. I liked it too. Did you knew that they made a new one?",
+        "No, I didn't knew that. Is it good?",
+        "Yes, it's very good. You should go see it."
+    ]
 
-    matches = evaluate_grammar(sentance)
-    text = '<h3>' + sentance + '</h3>'
-    for match in matches:
+    errors = evaluate_grammar(sentances)
 
-        # text += '<div>'
-        # text += '<p>' + match.category + '</p>'
+    html = "error num : " + str(len(errors))
+    for e in errors:
+
+        # print(match)
+
+        # html += '<div>'
+        # html += '<p>' + match.category + '</p>'
         # output = str(match)
-        # text += output.replace('\n', '<br>')
+        # html += output.replace('\n', '<br>')
 
-        # text += '</div>'
-        
-        if match.category in check_categories:
-            text += '<div>'
-            text += '<p>' + match.category + '</p>'
-            output = str(match)
-            text += output.replace('\n', '<br>')
+        # html += '</div>'
 
-            text += '</div>'
+        html += e.to_html()
 
-    return HttpResponse(text)
+    return HttpResponse(html)
 
-def evaluate_grammar(text):
+# texts : 発話した文章のリスト
+def evaluate_grammar(texts):
+
+    tool = language_tool_python.LanguageTool('en-US')
     
-    tool = language_tool_python.LanguageTool('en-US')  # use a local server (automatically set up), language English
-    
-    matches = tool.check(text)
+    errors = []
+    for i, text in enumerate(texts):
+        # 文法をチェック
+        matches = tool.check(text)
+        correct = tool.correct(text)
+
+        # 定められたカテゴリに合うものだけを記録
+        errors += [error(i, match, text)
+                    for match in matches if (match.category in check_categories)]
 
     tool.close()
 
-    return matches
+    return errors
 
