935040a696cc38ddd053dfaaa9e4219077c9138d
NR-Kyoto <nagaya.rei.62r@st.kyoto-u.ac.jp>
2023-05-16 16:22:42 +0900

    [update]単語追加（仮）

--- app/vocab/urls.py
+++ app/vocab/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.vocabpage, name='vocabpage'),
+    path('add', views.add_word, name='add'),
 ]
--- app/vocab/views.py
+++ app/vocab/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core import serializers
 from django.http import HttpResponse
 
-from vocab.models import Wordbook
+from .models import Wordbook
+from .forms import WordForm
 
 def vocabpage(request):
     posts = Wordbook.objects.all()
     post_list = serializers.serialize('json', posts)
     return HttpResponse(post_list, content_type="text/json-comment-filtered; charset=utf-8")
+
+# 単語追加
+def add_word(request):
+
+    # requestがPostならデータベースに追加
+    if request.method == "POST":
+        form = WordForm(request.POST)
+
+        # データがフォームに一致するかチェック
+        if form.is_valid():
+            form.save()
+            p = redirect('/vocab')
+            print(p)
+            return p
+
+    else:
+        # 単語帳のフォームを組み込んだページをレンダリング
+        form = WordForm()
+        return render(request, 'vocab/add_word.html', {'form': form})
+
+# 単語削除
+
+# 編集
 
