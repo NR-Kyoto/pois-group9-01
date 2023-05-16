6080d9bf648ecddc72e77629dda1c18254e55c12
kazuki3087 <matsumoto-kazuki-25@docomo.ne.jp>
2023-05-16 16:07:04 +0900

    [update] 音声読み上げ機能の追加

--- app/chat/views.py
+++ app/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import speech_recognition as sr
+from gtts import gTTS
 
 def chatpage(request):
     return HttpResponse("chat world")
@@ -19,3 +20,8 @@ def speech_to_text(audio_file):
         text = "Could not request results from Google Speech Recognition service"
 
     return text
+
+def text_to_speech(text):
+    message = text
+    tts = gTTS(message, lang='en')
+    tts.save('gTTS_test.mp3')
