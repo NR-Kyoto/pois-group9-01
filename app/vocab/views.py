9eb1de7a7b940278bf896fede116803cf9cd81d5
NR-Kyoto <nagaya.rei.62r@st.kyoto-u.ac.jp>
2023-05-17 18:26:44 +0900

    [update]単語から他の要素を自動で入力する機能を追加

--- app/vocab/views.py
+++ app/vocab/views.py
@@ -2,13 +2,34 @@ from django.shortcuts import render, redirect
 from django.core import serializers
 from django.http import HttpResponse
 
+from urllib.parse import urlencode
+
 from .models import Wordbook
 from .forms import WordForm
+from login.models import User
+
+from bs4 import BeautifulSoup # スクレイピング用
+import requests
+import re
+
+weblio_url='https://ejje.weblio.jp/content/'
 
 def vocabpage(request):
+    
+    if request.GET.get('w') != None:
+        dic = search_word(request.GET['w'])
+        dic['user_id'] = User.objects.all()[0]
+        form = Wordbook(**dic)
+        
+        # データがフォームに一致するかチェック
+        # if form.is_valid():
+        form.save()
+        return redirect('/vocab')
+
     posts = Wordbook.objects.all()
     post_list = serializers.serialize('json', posts)
-    return HttpResponse(post_list, content_type="text/json-comment-filtered; charset=utf-8")
+
+    return HttpResponse(post_list, content_type="application/json; charset=utf-8")
 
 # 単語追加
 def add_word(request):
@@ -42,3 +63,42 @@ def delete_word(request):
 
 # 編集
 
+# 英単語からの自動入力
+# weblioをスクレイピング
+def search_word(word):
+
+    # weblioで検索
+    response = requests.get(weblio_url+word)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # 発音記号
+    pronunciation = soup.find(class_='KejjeHt').get_text() if soup.find(class_='KejjeHt') else ''
+    
+    # 品詞
+    partofspeech = soup.find(class_='KnenjSub', id='KnenjPartOfSpeechIndex0').get_text() if soup.find(class_='KnenjSub', id='KnenjPartOfSpeechIndex0') else ''
+
+    # 日本語訳
+    japanese = ''
+    if soup.find(class_='lvlB'):
+        tmp = soup.find(class_='lvlB').get_text()
+        tmp1 = re.sub("《.*》", "", tmp)
+        japanese = re.sub("[.]", "", tmp1)
+    else:
+        lev0 = soup.find_all(class_='level0')
+
+        if len(lev0) != 0:
+            tmp1 = re.sub("《.*》", "", lev0[1].get_text())
+            japanese = re.sub("[.]", "", tmp1)
+
+    # 例文
+    example = soup.find(class_='KejjeYrEn').get_text() if soup.find(class_='KejjeYrEn') else ''
+
+    return {
+            "user_id": None,
+            "word": word,
+            "meaning": japanese,
+            "pronunciation": pronunciation,
+            "category": partofspeech,
+            "pronunciation": example,
+        }
+        
