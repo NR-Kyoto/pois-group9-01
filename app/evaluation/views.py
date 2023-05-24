3a72c7fdf2b90516a10cb88fbd40f2a59db0b209
NR-Kyoto <nagaya.rei.62r@st.kyoto-u.ac.jp>
2023-05-24 18:22:47 +0900

    [update]チャットページからデータを受け取り音声評価できるようにした

--- app/evaluation/views.py
+++ app/evaluation/views.py
@@ -1,48 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 
 from dataclasses import dataclass # 構造体
 from deep_translator import GoogleTranslator # 翻訳
 import language_tool_python # 文法チェック
-
 import re # 正規表現
 import json # ResponseをJsonで
+import os
 import collections
-import difflib
+# import difflib
+import base64, io, subprocess, tempfile
 
-from django.shortcuts import render
-from django.http import HttpResponse
-
-import json
 import azure.cognitiveservices.speech as speech_sdk
 
-import os
 from chat.models import Audio
-
-import base64, io, subprocess, tempfile
 
 # 認証情報を外部ファイルから読み出す
 COG_SERVICE_KEY=os.getenv('COG_SERVICE_KEY')
 COG_SERVICE_REGION=os.getenv('COG_SERVICE_REGION')
 
 check_categories = ['CONFUSED_WORDS', 'GRAMMAR', 'REPETITIONS', 'TYPOS']
-
-texts = [
-    "Hey, did you was at the party last night?",
-    "No, I didn't went. I was study for my exam.",
-    "Oh, that's right. I forgot about your exam. Did you did well?",
-    "Yes, I think I did good. I answered all of the questions correctly.",
-    "That's great! So, did you went home right after the exam?",
-    "No, I went to the mall to buy some clothes. I falled in love with a jacket that I saw there.",
-    "Ah, I see. Did you bought the jacket?",
-    "Yes, I did. It was expensive, but it was worth it.",
-    "Sounds like you had a busy day yesterday. I was stayed at home and watched TV all day.",
-    "That sounds relaxing. What did you watched?",
-    "I watched a movie called 'Jurassic Park'. It was very exciting.",
-    "Oh, I have saw that movie before. I liked it too. Did you knew that they made a new one?",
-    "No, I didn't knew that. Is it good?",
-    "Yes, it's very good. You should go see it."
-]
 
 @dataclass(init=False)
 class error:
@@ -106,47 +83,101 @@ class error:
             "error_message" : self.message,
             "fixed_sentence" : self.suggestion
         })
+    
         return dic
     
+# 評価
+# 文章なし -> 評価しない
+# 文章あり，音声なし -> 文法のみ
 def evaluationpage(request):
 
+    #---- Chatからのデータの受けとりと音声評価 ----#
+
+    # Chatページからデータを取得
     queryset = Audio.objects.order_by('id').values()
-    print(queryset)
-    texts = []
+
+    # データがなければチャットページにリダイレクト
+    if queryset.count() == 0:
+        return redirect('/chat/mock')
+
+    texts = [] # Chat-GPTに送ったテキスト
+    
+    # 音声の評価
+    speech_scores = {
+            "score": 0,
+            "accuracy_score": 0,
+            "fluency_score": 0,
+	        "completeness_score": 0,
+	        "words": []
+        }
+
     for item in queryset:
-        print("++++++++++++++++++++")
+
         text = item["text"]
-        print(item["text"])
+        if not text: continue
         texts.append(text)
+        
+        if item["fields"]["audio"] != None:
+
+            # webm -> wav
+            with tempfile.NamedTemporaryFile(delete=True, suffix="_webm") as audio_webm:
+                audio_webm.write(base64.b64decode(item["fields"]["audio"]))
+
+                with tempfile.NamedTemporaryFile(delete=True, suffix="_wav") as audio_wav:
+                    command = ['ffmpeg', '-y', '-i', str(audio_webm.name),'-f', 'wav', '-c:a', 'pcm_s16le', str(audio_wav.name)]
+                    
+                    # 変換ができたかチェック
+                    try:
+                        subprocess.run(command, check=True)
+
+                    except subprocess.CalledProcessError:
+                        continue
+                    
+                    # 音声評価
+                    try:
+                        result = evaluate_speech(text, audio_wav.name)
+                        
+                    except ValueError:
+                        continue
+
+                    speech_scores['score'] += result['score']
+                    speech_scores['accuracy_score'] += result['accuracy_score']
+                    speech_scores['fluency_score'] += result['fluency_score']
+                    speech_scores['completeness_score'] += result['completeness_score']
+                    speech_scores['words'] += result['words']
+
+    # 評価した内容をデータベースから削除
+    Audio.objects.all().delete()
+
+    # 単語ごとの発音評価でソート
+    sorted_words = sorted(speech_scores['words'], key=lambda x: x['score'])
+    
+
+    #---- 文法評価 ----#
+
+    # 文章がない場合はチャットページにリダイレクト
+    if not texts:
+        return redirect('/chat/mock')
 
     sentances = split_sentances(texts)
     errors = check_grammar(sentances)
+    grammar_score = get_grammar_score(len(sentances), errors) # 文法のスコア　0~5点
+    
 
-    # 文法のスコア　0~5点
-    grammar_score = get_grammar_score(len(sentances), errors)
-
-    # 音声の評価スコア
-    speech_json = evaluate_speech('What time is it now in Japan ?')
-    pronunciation_score = speech_json["score"]
-
-    # total_scoreの計算
-    total_score = grammar_score + pronunciation_score
-
+    #---- 送信データ ----#
     data = {
-        "total_score" : total_score,
+        "total_score" : grammar_score + speech_scores['score'],
         "grammar" : {
             "score" : grammar_score,
             "weaks" : grammar_weak_ranking(errors)
         },
         "speech" : {
-            "score" : pronunciation_score,
-            "weaks" : speech_json['words']
+            "score" : speech_scores['score'],
+            "weaks" : [sorted_words[i] for i in range(min(len(sorted_words), 5)) if sorted_words[i]['score'] != 5.0]
         }
     }
 
-    # return HttpResponse(html)
     return render(request, 'evaluation/evaluation.html', data)
-    # return HttpResponse(json.dumps(data, indent=2, ensure_ascii=False), content_type = "application/json; charset=utf-8")
 
 
 # 1文ずつに分割
@@ -171,8 +202,7 @@ def check_grammar(sentances):
         matches = tool.check(s)
 
         # 定められたカテゴリに合うものだけを記録
-        errors += [error(i, match)
-                    for match in matches if (match.category in check_categories)]
+        errors += [error(i, match) for match in matches if (match.category in check_categories)]
 
     tool.close()
 
@@ -203,15 +233,7 @@ def grammar_weak_ranking(errors):
 
 
 # 音声認識の評価
-
-# TODO 複数のwavファイルに対応
-def evaluate_speech(script):
-
-    # with tempfile.NamedTemporaryFile(delete=True, suffix="_webm") as audio_webm:
-    #     audio_webm.write(base64.b64decode(data))
-    #     #print(type(audio_webm))
-    #     with tempfile.NamedTemporaryFile(delete=True, suffix="_wav") as audio_wav:
-    #         command = ['ffmpeg', '-y', '-i', str(audio_webm.name),'-f', 'wav', '-c:a', 'pcm_s16le', str(audio_wav.name)]
+def evaluate_speech(script, path):
 
     # 評価パラメータ
     # 5段階評価，文・単語を評価
@@ -222,8 +244,7 @@ def evaluate_speech(script):
 
     # 諸設定
     speech_config = speech_sdk.SpeechConfig(COG_SERVICE_KEY, COG_SERVICE_REGION)
-    audioFile = 'evaluation/content/time.wav'
-    audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    audio_config = speech_sdk.AudioConfig(filename=path)
     speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
 
     # 音声を評価
@@ -231,28 +252,25 @@ def evaluate_speech(script):
     result = speech_recognizer.recognize_once()
 
     pronunciation_result = speech_sdk.PronunciationAssessmentResult(result)
-    # pronunciation_result_json = result.properties.get(speech_sdk.PropertyId.SpeechServiceResponse_JsonResult)
-    # print(pronunciation_result_json)
-    # print('Accuracy score: {}, fluency score: {}, completeness score : {}, pronunciation score: {}'.format(
-    #         pronunciation_result.accuracy_score, pronunciation_result.fluency_score,
-    #         pronunciation_result.completeness_score, pronunciation_result.pronunciation_score
-    #     ))
 
     words = []
-    for word in pronunciation_result.words:
-        words.append({
-            "word": word.word,
-            "score": word.accuracy_score
-        })
+    try:
+        for word in pronunciation_result.words:
+            words.append({
+                "word": word.word,
+                "score": word.accuracy_score
+            })
 
-    sorted_words = sorted(words, key=lambda x: x['score'])
+    # wordsがない＝音声の入っていないデータ
+    except AttributeError:
+        raise ValueError
 
     dic = dict({
             "score": pronunciation_result.pronunciation_score, # 総評的な点数
             "accuracy_score": pronunciation_result.accuracy_score, # 発音がどれだけ正しいのか
             "fluency_score": pronunciation_result.fluency_score, # 流暢さ（単語間隔など）
 	        "completeness_score": pronunciation_result.completeness_score,  # 正解文章に対してどれだけちゃんと発音したか＝ぬけがないか
-	        "words": [sorted_words[i] for i in range(5) if sorted_words[i]['score'] != 5.0]
+	        "words": words
         })
 
     return dic
